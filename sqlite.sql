4575351c6eed430f14cd3f341cb6c0ba9e4e7437
Jim Northrup <jim@vsiwest.com>
2020-04-27 16:57:35 +0700

    while no PK is plausible for the dataset, we did rtfm the vaccuum option.

--- sqlite.sql
+++ sqlite.sql
@@ -1,4 +1,4 @@
--- rm /tmp/sa.sqlite ;time sqlite3 /tmp/sa.sqlite <sqlite.sql ;sqlite3 /tmp/sa.sqlite "select * from  superannuated1909 limit 2;"; ls -lh superannuated1909.fwf  /tmp/sa.sqlite
+-- rm /tmp/sa.sqlite ;time sqlite3 /tmp/sa.sqlite <sqlite.sql ;echo -e ".mode column\n.headers on\nselect * from  superannuated1909 limit 2;" |sqlite3 /tmp/sa.sqlite ; ls -lh superannuated1909.fwf  /tmp/sa.sqlite
 
 
 CREATE TABLE fixed_width_table
@@ -7,9 +7,21 @@ CREATE TABLE fixed_width_table
 );
 
 .import superannuated1909.fwf fixed_width_table
+CREATE TABLE superannuated1909
+(
+    'SalesNo'     INT,
+    'SalesAreaID' TEXT,
+    'date'        TEXT,
+    'PluNo'       TEXT,
+    'ItemName'    TEXT,
+    'Quantity'    FLOAT,
+    'Amount'      FLOAT,
+    'TransMode'   TEXT
+);
 
-CREATE VIEW superannuated1909 as
-Select CAST(SUBSTR(full_string, 1 + 0, 11 - 0) as NUMERIC)  AS 'SalesNo',
+-- CREATE TABLE superannuated1909 as
+INSERT INTO superannuated1909
+SELECT CAST(SUBSTR(full_string, 1 + 0, 11 - 0) as NUMERIC)  AS 'SalesNo',
        RTRIM(SUBSTR(full_string, 1 + 11, 15 - 11))          AS 'SalesAreaID',
        DATE(SUBSTR(full_string, 1 + 15, 25 - 15))           AS 'date',
        RTRIM(SUBSTR(full_string, 1 + 25, 40 - 25))          AS 'PluNo',
@@ -19,8 +31,12 @@ Select CAST(SUBSTR(full_string, 1 + 0, 11 - 0) as NUMERIC)  AS 'SalesNo',
        RTRIM(SUBSTR(full_string, 1 + 103, 108 - 103))       AS 'TransMode'
 FROM fixed_width_table;
 
-
+DROP TABLE fixed_width_table;
+VACUUM;
 --
 -- jdbc2json  
 --  TYPES=['"VIEW"'] ../../jdbc2json/bin/jdbctocouchdbbulk.sh http://127.0.0.1:5984/foo1_ jdbc:sqlite:/tmp/sa.sqlite
+
+-- bitbanging
+--  BULKSIZE=5 LIMIT=10 TERSE=true  ../../jdbc2json/bin/jdbctocouchdbbulk.sh http://127.0.0.1:9989/foo1_ jdbc:sqlite:/tmp/sa.sqlite
 
